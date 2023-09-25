0273b965a2c99388d273608812bf19ac9c3d6efd
Solomon-H <solomonhagos1980@gmail.com>
2023-09-25 13:29:38 -0400

    changed the name of the app

--- src/main/java/VacationApp.java
+++ src/main/java/VacationApp.java
@@ -1,7 +1,7 @@
 import java.util.List;
 import java.util.Scanner;
 
-public class HotelReservation {
+public class VacationApp {
     public static void main(String[] args) {
         Hotel hotel = new Hotel(10);
         Scanner scanner = new Scanner(System.in);
